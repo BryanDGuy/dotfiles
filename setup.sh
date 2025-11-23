77b289f222042ef71674eedba1658b912d7fd24b
Bryan Dougherty <doughertybryan13@gmail.com>
2025-11-22 19:22:51 -0600

    Adding wallpapers

--- setup.sh
+++ setup.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+
+# Function to prepare directory for fresh copy
+prepare_dir() {
+    local dest_dir="$1"
+    if [ -d "$dest_dir" ]; then
+        # Directory exists, remove all contents
+        rm -rf "$dest_dir"/*
+    else
+        # Create directory
+        mkdir -p "$dest_dir"
+    fi
+}
+
+# Prepare and copy wallpapers
+prepare_dir "$HOME/wallpapers"
+cp -r wallpapers/* "$HOME/wallpapers/"
+
 cp hyprland.conf $HOME/.config/hypr/
 cp hyprpaper.conf $HOME/.config/hypr/
