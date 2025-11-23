e58e7647a564d71d68505de3aebbeeee7e7e9948
Bryan Dougherty <doughertybryan13@gmail.com>
2025-11-22 19:45:58 -0600

    Not deleting files

--- setup.sh
+++ setup.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# Function to prepare directory for fresh copy
-prepare_dir() {
-    local dest_dir="$1"
-    if [ -d "$dest_dir" ]; then
-        # Directory exists, remove all contents
-        rm -rf "$dest_dir"/*
-    else
-        # Create directory
-        mkdir -p "$dest_dir"
-    fi
-}
+mkdir -p $HOME/.config/hypr
+cp -r hypr/* $HOME/.config/hypr/
 
-# Prepare and copy wallpapers
-prepare_dir "$HOME/wallpapers"
-cp -r wallpapers/* "$HOME/wallpapers/"
-
-cp hyprland.conf $HOME/.config/hypr/
-cp hyprpaper.conf $HOME/.config/hypr/
+mkdir -p $HOME/.config/waybar
+cp -r waybar/* $HOME/.config/waybar/
